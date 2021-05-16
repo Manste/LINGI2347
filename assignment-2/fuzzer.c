69ab81bc50dbcae35112ad4696e32c83c62a3f83
Manste <manuelle.ndamtang@yahoo.com>
2021-05-17 01:18:21 +0200

    found 4 error

--- assignment-2/fuzzer.c
+++ assignment-2/fuzzer.c
@@ -12,8 +12,6 @@
 #define BLOCKING_FACTOR 20
 #define RECORDSIZE      10240
 
-
-
 #define TMAGIC   "ustar"        /* ustar and a null */
 #define TMAGLEN  6
 #define TVERSION "00"           /* 00 and no null */
@@ -34,24 +32,10 @@
                                    next file in the archive */
 #define XGLTYPE  'g'            /* Global extended header */
 
-/* Bits used in the mode field, values in octal.  */
-#define TSUID    04000          /* set UID on execution */
-#define TSGID    02000          /* set GID on execution */
-#define TSVTX    01000          /* reserved */
-                                /* file permissions */
-#define TUREAD   00400          /* read by owner */
-#define TUWRITE  00200          /* write by owner */
-#define TUEXEC   00100          /* execute/search by owner */
-#define TGREAD   00040          /* read by group */
-#define TGWRITE  00020          /* write by group */
-#define TGEXEC   00010          /* execute/search by group */
-#define TOREAD   00004          /* read by other */
-#define TOWRITE  00002          /* write by other */
-#define TOEXEC   00001          /* execute/search by other */
+// This are the different typeflags used to 
+const char typeflags[] = {REGTYPE, AREGTYPE, LNKTYPE, SYMTYPE, CHRTYPE, BLKTYPE, DIRTYPE, FIFOTYPE, CONTTYPE, XHDTYPE, XGLTYPE};
 
-const unsigned typeflags[] = {REGTYPE, AREGTYPE, LNKTYPE, SYMTYPE, CHRTYPE, BLKTYPE, DIRTYPE, FIFOTYPE, CONTTYPE, XHDTYPE, XGLTYPE};
-const mode_t modes[] = {04000, 02000, 01000, 00400, 00200, 00100, 00040, 00020, 00010, 00004, 00002, 00001};
-
+// The structure of the archive header
 struct tar_header {               /* byte offset */
     char name[100];               /*   0 */
     char mode[8];                 /* 100 */
@@ -72,6 +56,7 @@ struct tar_header {               /* byte offset */
     char padding[12];             /* 500 */
 };
 
+//
 struct tar_t 
 {
     unsigned pos;
@@ -89,86 +74,117 @@ int main(int argc, char* argv[])
         printf("Failure: There are no arguments\n");
         return -1;
     }
+
     int rv = 0;
-    char cmd[51];
+    char cmd[61];
     strncpy(cmd, argv[1], 25);
     cmd[26] = '\0';
-    int nbfound = 0, nbmax = 3, cpt = 0;
-    while (nbfound <=5 & cpt < nbmax) {
+    int nbmax = 270;
+
+    for (int cpt = 0; cpt < nbmax; cpt++) {
         char *id;
         char archive[15];
-        snprintf(archive, 15, "archive%d.tar", cpt);
-        if (cpt == 1) {
-            if_512_bytes(archive);
-        }
-        else if (cpt == 0)
-        {
-            if_data_failed(archive);
-        }
-        else if (cpt == 2) 
-        {
-            create_correct_tar_files(archive, DIRTYPE, 0664);
-        }
-        
-        
-        char for_command[strlen(archive) + 1];
+        snprintf(archive, 22, "archive%d.tar", cpt);
 
-        strcpy(for_command, " ");
-        strcat(for_command, archive);
-        strncat(cmd, for_command, 25);
-        char buf[37];
-        FILE *fp;
-
-        if ((fp = popen(cmd, "r")) == NULL) {
-            printf("Error opening pipe!\n");
-            return -1;
-        }
-
-        if(fgets(buf, 37, fp) == NULL) {
-            printf("No output\n");
-            goto finally;
-        }
-        if(strstr(buf, "*** The program has crashed ***\n")) {
-            nbfound++;
-            char new_name[30];
-            strcpy(new_name, "success_");
-            strcat(new_name, archive);
-
-            // success file already exists, remove it
-            if( access( new_name, F_OK ) == 0 ) {
-                remove(new_name);
-            } 
-            // copy the successful file into another file
-            FILE *fptr1, *fptr2;
-            fptr1 = fopen(archive, "r");
-            fptr2 = fopen(new_name, "w");
-            char c = fgetc(fptr1);
-            while (c != EOF ) {
-                fputc(c, fptr2);
-                c = fgetc(fptr1);
+        // We are using cloned process to execute the command
+        int pid = fork();
+        if (pid == 0) {
+            
+            if (cpt == 0) {
+                // This will create an archive file without completing adding at the end of the archive file two 512-byte blocks filled with binary zeros as an end-of-file marker
+                if_512_bytes(archive);
             }
-            fclose(fptr1);
-            fclose(fptr2); 
-            printf("*** The program has crashed with ***\n");  
-            goto finally;
-        } else {
-            printf("Crash message\n");
-            rv = 1;
-            goto finally;
+            else if (cpt == 1)
+            {
+                // Here, we create a archive file specifying, there exist a file test.txt but no data exist for the archive file.
+                if_data_failed(archive);
+            }
+            else  if (cpt - 2 < 11)
+            {
+                // Try possible typeflags settings
+                create_correct_tar_files(archive, typeflags[cpt - 2], 0664);
+            }
+            else if (cpt - 13 < 255) {
+                char txt[cpt] ;
+                random_strings(cpt, txt);
+                // To try out type with possible string for the type 
+                create_correct_tar_files(archive, txt, 0664);
+            } 
+            else if (cpt = 268) {
+                // To try using wronly pointer and addresses for header
+                test(archive);
+            }
+            else {
+                // Try with many text file in the archive file
+                create_correct_tar_multiple_files(archive, REGTYPE, 0664, cpt);
+            }
+            
+            char for_command[strlen(archive) + 1];
+
+            strncpy(for_command, " ", 2);
+            strncat(for_command, archive, 15);
+            strncat(cmd, for_command, 25);
+            char buf[33];
+            FILE *fp;
+
+            if ((fp = popen(cmd, "r")) == NULL) {
+                printf("Error opening pipe!\n");
+                return -1;
+            }
+
+            if(fgets(buf, sizeof buf, fp) == NULL) {
+                printf("No output\n");
+                goto finally;
+            }
+            if(strcmp(buf, "*** The program has crashed ***\n") == 0) {
+                char new_name[30];
+                strcpy(new_name, "success_");
+                strcat(new_name, archive);
+
+                // success file already exists, remove it
+                if( access( new_name, F_OK ) == 0 ) {
+                    remove(new_name);
+                } 
+                // copy the successful archive file into another file(sucess_archive)
+                FILE *fptr1, *fptr2;
+                fptr1 = fopen(archive, "r");
+                fptr2 = fopen(new_name, "w");
+                char c = fgetc(fptr1);
+                while (c != EOF ) {
+                    fputc(c, fptr2);
+                    c = fgetc(fptr1);
+                }
+                fclose(fptr1);
+                fclose(fptr2); 
+                printf("*** The program has crashed with %s ***\n", new_name);  
+                goto finally;
+            } else {
+                printf("Crash message\n");
+                rv = 1;
+                goto finally;
+            }
+            finally:
+            if(pclose(fp) == -1) {
+                printf("Command not found\n");
+                rv = -1;
+            }
+            return 0;
         }
-        finally:
-        if(pclose(fp) == -1) {
-            printf("Command not found\n");
-            rv = -1;
+        else if(pid != -1){ // The parent is waiting
+            wait(NULL);
         }
-        
-        cpt++;
+        else {
+            perror("Error while calling the fork function");
+        }
     }
-    printf("We found  %d crashes\n", nbfound);
     return rv;
 }
 
-// To calculate the checksum
+/* 
+    To calculate the checksum
+
+    -> struct tar_header * entry:  the struct used to save the stream for the creation of the tar file
+*/
 unsigned int calculate_checksum(struct tar_header * entry){
      memset(entry->chksum, ' ', 8);
     
@@ -185,7 +201,12 @@ unsigned int calculate_checksum(struct tar_header * entry){
     return check;
 }
 
-// To generate random integer: This is useful to generate random string 
+/*
+    To generate random integer: This is useful to generate random string 
+
+    -> int start: The min value
+    -> int end: The max value
+*/
 int random_number(int start, int end) {
     int result = 0;
     int min_num = 0;
@@ -201,8 +222,13 @@ int random_number(int start, int end) {
     return result;
 }
 
-// To generate random string for our text-file
-void random_strings(size_t length, char *randomString) { // const size_t length, supra
+/*
+    To generate random string for our text-file
+
+    -> size_t length: The size defined for our string
+    -> char *randomString: The pointer to memory allocated to the string
+*/
+void random_strings(size_t length, char *randomString) {
 
     static char charset[] = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
 
@@ -220,8 +246,9 @@ void random_strings(size_t length, char *randomString) { // const size_t length,
 
 /* 
     To open the stream file
-    *tar: the struct data used to save the structure of the tar file
-    *filename: the filename of the archive
+
+    -> struct tar_t *tar: the struct data used to save the structure of the tar file
+    -> char *filename: the filename of the archive
 */
 void prepare_tar(struct tar_t *tar, char *filename){
     char mode[] = "wb";
@@ -231,15 +258,20 @@ void prepare_tar(struct tar_t *tar, char *filename){
 
 /*
     This function elaborate the content of the archive header, complete its content and save it in the stream file.
-    mode: the mode used during the initiation of the 
+    
+    -> unsigned mode: the mode used during the initiation of the tar file
+    -> unsigned size: the size of the file
+    -> unsigned type: the type of the tar_header
+    -> unsigned *name: the name of the tar file
+    -> struct tar_t *tar: the struct used to save the stream for the creation of the tar file
+    -> struct tar_header hdr: the struct given the header information
 */
-int tar_write_header(struct tar_t *tar, char *name, unsigned size, mode_t mode, char type){
-    struct tar_header hdr;
+int tar_write_header(struct tar_t *tar, struct tar_header hdr, char *name, unsigned size, unsigned mode, unsigned type){
 	struct passwd *pw = getpwuid(getuid());
 	struct group *gr = getgrgid(getgid());
     memset(&hdr, 0, sizeof(struct tar_header));
     strncpy(hdr.name, name, 100);
-    snprintf(hdr.mode, sizeof(hdr.mode), "%07o", mode & 0777);
+    snprintf(hdr.mode, sizeof(hdr.mode), "%07o", mode );
     snprintf(hdr.uid, sizeof(hdr.uid), "%07o", getuid());
     snprintf(hdr.gid, sizeof(hdr.gid), "%07o", getgid());
     snprintf(hdr.uname, sizeof(hdr.uname), "%s", pw->pw_name);
@@ -263,43 +295,68 @@ int tar_write_header(struct tar_t *tar, char *name, unsigned size, mode_t mode, 
 		snprintf(hdr.devmajor,  8, "%07o", 0U);
 		snprintf(hdr.devminor,  8, "%07o", 0U);
 	} 
-    else if(not_in_array(type, typeflags) ) {
-        printf("Failure: unable to determine the filetype\n");
-        return -1;
-    }
     calculate_checksum(&hdr);
     tar->remaining_data = hdr.size;
     fwrite(&hdr, sizeof(hdr), 1, tar->stream);
     return 0;
 }
+/*
+    We are wrong pointer for the header
 
-// To check if an array contains a value
-int not_in_array(unsigned *element, unsigned arr[]) {
-    int n = sizeof(arr)/sizeof(mode_t);
-    for (int i = 0; i < n; i++) {
-        if (arr[i] == element) {
-            return 0;
-        }
-    }
-    return 1;
+    -> char *name: the name of the archive file
+*/
+void test(char * name) {
+    const size_t size = 2000;
+    char *text = malloc(sizeof(char) * (size +1));
+    random_strings(size, text);
+    
+    struct tar_t tar;
+    struct tar_header hdr;
+
+    prepare_tar(&tar, name);
+    complete_header_only(&tar, hdr, "test.txt", strlen(text), 0664, REGTYPE);
+    calculate_checksum(&hdr);
+    tar.remaining_data = hdr.size;
+    fwrite(&hdr, sizeof(hdr), 1, tar.stream);
+    tar_write_data(&tar, text, strlen(text));
+    tar_finalize(&tar);
 }
 
-// Write the value or the content of a file
+/* 
+    Write the value or the content of a file
+
+    -> struct tar_t *tar: 
+    -> void *data: The pointer for the data file
+    -> unsigned size: 
+*/
 int tar_write_data(struct tar_t *tar, void *data, unsigned size) {
     fwrite(data, size, 1, tar->stream); 
     tar->remaining_data -= size;
     if (tar->remaining_data == 0) {
-        return write_null_bytes(tar, (512 - tar->pos% 512)%512);
+        return write_null_bytes(tar, (512 - tar->pos% 512)%512); // With "(512 - tar->pos% 512)%512" we calculate the number zero bytes to complete the file
     }
     return 0;
 }
 
+/*
+    This function finalize the creation of tar file adding at the end of the archive file 
+    two 512-byte blocks filled with binary zeros as an end-of-file marker. In the second part 
+    of the function, we just close the stream creating the archive file.
+    
+    -> struct tar_t *tar: contains the stream for the creation of archive file.
+*/
 int tar_finalize(struct tar_t *tar) {
     write_null_bytes(tar, sizeof(struct tar_header) * 2);
     fclose(tar->stream);
     return 0;
 }
 
+/*
+    This function complete the stream creating the archive file.
+
+    -> struct tar_t *tar: contains the stream for the creation of archive file.
+    -> int n: The number of necessary to complete the file
+*/
 int write_null_bytes(struct tar_t *tar, int n) {
     int i, err;
     char nul = '\0';
@@ -312,48 +369,135 @@ int write_null_bytes(struct tar_t *tar, int n) {
     return 0;
 } 
 
-void removeSpaces(char *str) {
-    int count = 0;
-    for (int i = 0; str[i]; i++)
-        if (str[i] != ' ')
-            str[count++] = str[i];
-    str[count] = '\0';
-}
+/*
+    This function create a simple archive file with one text file.
 
+    -> char *name: The name of the archive file
+    -> unsigned typeflag: the typeflag assigned to the archive file
+    -> unsigned mode: The mode  fixed for the archive file
+*/
 void create_correct_tar_files(char *name, unsigned typeflag, unsigned mode){
 
+    // Create text for simple file
     const size_t size = 2000;
     char *text = malloc(sizeof(char) * (size +1));
     random_strings(size, text);
     
     struct tar_t tar;
+    struct tar_header hdr;
 
     prepare_tar(&tar, name);
-    tar_write_header(&tar, "test.txt", strlen(text), mode, typeflag);
+    // Save a text file 
+    tar_write_header(&tar, hdr, "test.txt", strlen(text), mode, typeflag);
     tar_write_data(&tar, text, strlen(text));
     tar_finalize(&tar);
 }
 
+/*
+    This function create an archive file without completing adding at the end of the archive file 
+    two 512-byte blocks filled with binary zeros as an end-of-file marker.
+
+    -> char *name: the name of the archive file
+*/
 void if_512_bytes(char *name) {
     const size_t size = 2000;
     char *text = malloc(sizeof(char) * (size +1));
     random_strings(size, text);
     
     struct tar_t tar;
+    struct tar_header hdr;
 
     prepare_tar(&tar, name);
-    tar_write_header(&tar, "test.txt", strlen(text), 0664, REGTYPE);
+    tar_write_header(&tar, hdr, "test.txt", strlen(text), 0664, REGTYPE);
     tar_write_data(&tar, text, strlen(text));
 }
 
+/*
+    This function create a archive file that create an entry for data without saving the 
+    corresponding data to that file.
+
+    -> char *name: the name of the archive file
+*/
 void if_data_failed(char *name) {
     const size_t size = 2000;
     char *text = malloc(sizeof(char) * (size +1));
     random_strings(size, text);
     
     struct tar_t tar;
+    struct tar_header hdr;
 
     prepare_tar(&tar, name);
-    tar_write_header(&tar, "test.txt", strlen(text), 0664, REGTYPE);
+    tar_write_header(&tar, hdr, "test.txt", strlen(text), 0664, REGTYPE);
     tar_finalize(&tar);
+}
+
+/*
+    This function create a simple archive file with multiples files.
+
+    -> char *name: The name of the archive file
+    -> unsigned typeflag: the typeflag assigned to the archive file
+    -> unsigned mode: The mode  fixed for the archive file
+    -> int nb: The number of text files
+*/
+void create_correct_tar_multiple_files(char *name, unsigned typeflag, unsigned mode, int nb){
+    
+    struct tar_t tar;
+    struct tar_header hdr;
+
+    prepare_tar(&tar, name);
+    // Save the text files
+    for (int i = 0; i < nb; i++) {
+        // Create text for simple file
+        const size_t size = 2000;
+        char *text = malloc(sizeof(char) * (size +1));
+        random_strings(size, text);
+
+        char *text_file = malloc(15);
+        snprintf(text_file, 22, "text%d.txt", i);
+        tar_write_header(&tar, hdr, *text_file, strlen(text), mode, typeflag);
+        tar_write_data(&tar, text, strlen(text));
+    }
+    tar_finalize(&tar);
+}
+
+/*
+    Here we are only complete the tar-header
+
+    > unsigned mode: the mode used during the initiation of the tar file
+    -> unsigned size: the size of the file
+    -> unsigned type: the type of the tar_header
+    -> unsigned *name: the name of the tar file
+    -> struct tar_t *tar: the struct used to save the stream for the creation of the tar file
+    -> struct tar_header hdr: the struct given the header information
+*/
+int complete_header_only(struct tar_t *tar, struct tar_header hdr, char *name, unsigned size, unsigned mode, unsigned type) {
+    struct passwd *pw = getpwuid(getuid());
+	struct group *gr = getgrgid(getgid());
+    memset(&hdr, 0, sizeof(struct tar_header));
+    strncpy(hdr.name, name, 100);
+    snprintf(hdr.mode, sizeof(hdr.mode), "%07o", mode );
+    snprintf(hdr.uid, sizeof(hdr.uid), "%07o", getuid());
+    snprintf(hdr.gid, sizeof(hdr.gid), "%07o", getgid());
+    snprintf(hdr.uname, sizeof(hdr.uname), "%s", pw->pw_name);
+    snprintf(hdr.gname, sizeof(hdr.gname), "%s", gr->gr_name);
+    snprintf(hdr.mtime, sizeof(hdr.mtime), "%011o", (int) time(NULL));
+    memcpy(hdr.version, TVERSION, 2);
+    memcpy(hdr.magic, TMAGIC, 6);
+    memset(hdr.size, '0', 12);
+    hdr.typeflag = type;
+    if(type == REGTYPE) {
+		snprintf(hdr.size, sizeof(hdr.size), "%011o", (unsigned)size);
+	} 
+    else if(type == SYMTYPE) {
+		readlink(name, hdr.linkname, 100);
+	} 
+    else if(type == CHRTYPE) {
+		snprintf(hdr.devmajor,  8, "%07o", 0U);
+		snprintf(hdr.devminor,  8, "%07o", 0U);
+	} 
+    else if(type == BLKTYPE) {
+		snprintf(hdr.devmajor,  8, "%07o", 0U);
+		snprintf(hdr.devminor,  8, "%07o", 0U);
+	} 
+    return 0;    
 }
